0f72ba5a6b4a87b9b714158c6d5ad92b667bc207
zima2020 <60121247+zima2020@users.noreply.github.com>
2020-01-27 14:38:58 -0600

    Minor updates

--- LandAreasCaseStudy/src/BarrenLandAnalysisMain.java
+++ LandAreasCaseStudy/src/BarrenLandAnalysisMain.java
@@ -5,84 +5,73 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class BarrenLandAnalysisMain {
-// Global Field object.
+
+	// Global Field object.
 	private static Field myField = new Field();
+
 	public static void main(String[] args) throws IOException {
-		
+
 		// Create and initialize the field
 		myField.zeroMatrix();
-		
+
 		// Parse user input
 		System.out.println("Please enter a set of four numbers, or enter 'q' to exit: ");
-		// Gather input until we've received valid input.
 		readInput();
-	
-		// Calculate arable land sections
+
+		// Calculate non-barren land sections
 		myField.calculatefarmableLand();
-		
+
 		// Print results to the console.
 		myField.printGrid();
 
 		System.exit(0);
 	}
-	
-	
-	public static void assertRange(Field land, String[] coordinates) {
-		
-        if (( new Integer(coordinates[0] )>= Field.xLimit) || new Integer(coordinates[2]) >= Field.xLimit) {
-        	
-            throw new IllegalArgumentException(
-                    "Point not found. This farm has width of:" + Field.xLimit);
-        }
 
-        if (( new Integer(coordinates[1]) >= Field.yLimit) ||  (new Integer(coordinates[3])>= Field.yLimit)) {
-            throw new IllegalArgumentException(
-                    "Point not found. This farm has height of:" + Field.yLimit);
-        }
+	// makes sure input coordinates are within the field x y range
+	public static void assertRange(Field land, String[] coordinates) {
+
+		if ((new Integer(coordinates[0]) >= Field.xLimit) || new Integer(coordinates[2]) >= Field.xLimit) {
+
+			throw new IllegalArgumentException(
+					"Input coordinate is larger then given field size. This farm has width of:" + Field.xLimit);
+
+		}
+
+		if ((new Integer(coordinates[1]) >= Field.yLimit) || (new Integer(coordinates[3]) >= Field.yLimit)) {
+			throw new IllegalArgumentException(
+
+					"Input coordinate is larger then given field size. This farm has height of:" + Field.yLimit);
+		}
 	}
 
-	
-  /*  public static boolean validateRange(Field farm, String[] input) {
-        try {
-            assertRange(farm, input);
-            return true;
-        } catch (IllegalArgumentException e) {
-            return false;
-        }
-    }*/
-	
-	public static void readInput() throws IOException{
-		
+	// Reads and validates input
+	public static void readInput() throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		String stop  =  "q";
-		if(s.equals(stop)){
+		String stop = "q";
+		if (s.equals(stop)) {
 			System.out.println("System will now exit.");
 			System.exit(0);
 		}
 		String[] parts = s.split(",");
-		
-	    for(int i = 0; i < parts.length; i++){
-	    	String[] numbers = parts[i].replaceAll("[^0-9]+", " ").trim().split(" ");
-	    	
-	    	if(numbers.length == 4){
-	    		
-	    		assertRange(myField, numbers);
-	    		myField.setBarren(new Point(Integer.parseInt(numbers[0]),  Integer.parseInt(numbers[1])), 
-	    						  new Point(Integer.parseInt(numbers[2]),  Integer.parseInt(numbers[3])));
-	    		} else {
-	    		System.out.println("ERROR: " + Arrays.toString(numbers) + " needs to have exactly 4 numbers." + '\n' +
-	    							"Enter in a valid set of numbers, or enter 'q' to exit.");
-	    	
-	    		}
-	
-	    }
+
+		for (int i = 0; i < parts.length; i++) {
+			String[] numbers = parts[i].replaceAll("[^0-9]+", " ").trim().split(" ");
+
+			if (numbers.length == 4) {
+
+				assertRange(myField, numbers);
+
+				myField.setBarren(new Point(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1])),
+						new Point(Integer.parseInt(numbers[2]), Integer.parseInt(numbers[3])));
+			} else {
+
+				System.out.println("ERROR: " + Arrays.toString(numbers) + " needs to have exactly 4 numbers." + '\n'
+						+ "Enter in a valid set of numbers, or enter 'q' to exit.");
+
+			}
+
+		}
 	}
 }
-
-
-	
-
-
-
-
--- LandAreasCaseStudy/src/Field.java
+++ LandAreasCaseStudy/src/Field.java
@@ -7,75 +7,74 @@ public class Field {
 	// Queue used to perform BFS
 	private LinkedList<Point> queue = new LinkedList<Point>();
 	// Horizontal bounds
-	public static  int xLimit = 400;
+	public static int xLimit = 400;
 	// Vertical bounds
 	public static int yLimit = 600;
 	// 2D array representing the 400 X 600 grid.
 	int landMap[][] = new int[xLimit][yLimit];
 	// HashMap of farmable land
 	HashMap<Integer, Integer> farmableLandMap = new HashMap<Integer, Integer>();
-	
-	//initialize array to all 0
-	public void zeroMatrix(){
-		for(int i = 0; i < xLimit; i++){
-			for(int j = 0; j < yLimit; j++){
+
+	// initialize array to all 0
+	public void zeroMatrix() {
+		for (int i = 0; i < xLimit; i++) {
+			for (int j = 0; j < yLimit; j++) {
 				landMap[i][j] = 0;
 			}
 		}
 	}
-	
+
 	// Traverse the entire area of the rectangle, and mark it 1 for barren.
-	public void setBarren(Point lowerLeft, Point upperRight){
-		for(int i = lowerLeft.x; i <= upperRight.x; i++){
-			for(int j = lowerLeft.y; j <= upperRight.y; j++){
-				
+	public void setBarren(Point lowerLeft, Point upperRight) {
+		for (int i = lowerLeft.x; i <= upperRight.x; i++) {
+			for (int j = lowerLeft.y; j <= upperRight.y; j++) {
+
 				landMap[i][j] = 1;
 			}
 		}
-		
+
 	}
-	
-	
-	//adds adjacent points to the queue of points to be checked
-	public void addAdjacent(int x, int y){
-		if(x > 0){
-			if(landMap[x-1][y] == 0){
-				queue.add(new Point(x-1, y));
+
+	// adds adjacent points to the queue of points to be checked
+	public void addAdjacent(int x, int y) {
+		if (x > 0) {
+			if (landMap[x - 1][y] == 0) {
+				queue.add(new Point(x - 1, y));
 			}
 		}
-		if(x < (xLimit - 1)){
-			if(landMap[x+1][y] == 0){
-				queue.add(new Point(x+1, y));
+		if (x < (xLimit - 1)) {
+			if (landMap[x + 1][y] == 0) {
+				queue.add(new Point(x + 1, y));
 			}
 		}
-		if(y > 0){
-			if(landMap[x][y-1] == 0){
-				queue.add(new Point(x, y-1));
+		if (y > 0) {
+			if (landMap[x][y - 1] == 0) {
+				queue.add(new Point(x, y - 1));
 			}
 		}
-		if(y < (yLimit - 1)){
-			if(landMap[x][y+1] == 0){
-				queue.add(new Point(x, y+1));
+		if (y < (yLimit - 1)) {
+			if (landMap[x][y + 1] == 0) {
+				queue.add(new Point(x, y + 1));
 			}
 		}
-		
+
 	}
-	
-	//implementation of the BFS traversal 
-	public void  calculatefarmableLand(){
+
+	// implementation of the BFS traversal
+	public void calculatefarmableLand() {
 		int farmableLand = 1;
 		int i = 0;
 		int j = 0;
-		
-		while(i < xLimit && j < yLimit){
-			if(queue.isEmpty()){
-				Point coordinate = new Point(i,j);
-				if(landMap[i][j] == 0){
+
+		while (i < xLimit && j < yLimit) {
+			if (queue.isEmpty()) {
+				Point coordinate = new Point(i, j);
+				if (landMap[i][j] == 0) {
 					farmableLand++;
 					farmableLandMap.put(farmableLand, 0);
 					queue.add(coordinate);
 				}
-				if(i == (xLimit - 1)){
+				if (i == (xLimit - 1)) {
 					// Move to the next row
 					i = 0;
 					j++;
@@ -84,46 +83,52 @@ public class Field {
 					i++;
 				}
 			}
-			if (!queue.isEmpty()){
+			if (!queue.isEmpty()) {
 				Point farmableCoordinate = queue.pop();
 				int x = farmableCoordinate.x;
 				int y = farmableCoordinate.y;
-				
-				if(landMap[x][y] == 0){
+
+				if (landMap[x][y] == 0) {
 					// Add adjacent points to the queue to be checked.
-					addAdjacent(x,y);
+					addAdjacent(x, y);
 					// Mark this location as checked.
 					landMap[x][y] = farmableLand;
 					// increment the value of this section of farmable land.
-					farmableLandMap.put(farmableLand, farmableLandMap.get(farmableLand) + 1);		
+					farmableLandMap.put(farmableLand, farmableLandMap.get(farmableLand) + 1);
 				}
 			}
-	
+
 		}
 	}
-	
-	//
-	public void printGrid(){
-		int i = 0 ;
+
+	// prints farmable land areas to the system output
+
+	public void printGrid() {
+
+		int i = 0;
 		int[] farmableLandPieces = new int[farmableLandMap.values().size()];
-		for (HashMap.Entry<Integer, Integer> entry : farmableLandMap.entrySet()){
-			try{
+
+		for (HashMap.Entry<Integer, Integer> entry : farmableLandMap.entrySet()) {
+			try {
 				farmableLandPieces[i] = entry.getValue();
 				i++;
-			} catch(ArrayIndexOutOfBoundsException e){
-				
+			} catch (ArrayIndexOutOfBoundsException e) {
+
 				e.getMessage();
 			}
 		}
-	
+
+		if (farmableLandPieces.length == 0) {
+			// No fertile land was found
+			System.out.print("No fertile land found");
+		}
+
+		else {
 			Arrays.sort(farmableLandPieces);
 			String output = Arrays.toString(farmableLandPieces).replaceAll("\\[|\\]|,", " ");
 			System.out.println(output);
-		
+		}
 
 	}
-	
-	
-		
-}
 
+}
